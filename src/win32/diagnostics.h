1c6a98815c544d1602c90af3586c586040fb7f40
Paweł Czerwiński <22awik@gmail.com>
2024-10-25 15:50:37 +0200

    Simple window

--- src/win32/diagnostics.h
+++ src/win32/diagnostics.h
@@ -26,18 +26,18 @@ void SetupDiagnostics()
 
 wchar_t* _convertToUtf16(const char * utf8)
 {
-    int size_needed = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, NULL, 0);
-    wchar_t* utf16 = (wchar_t*) malloc(size_needed * sizeof(wchar_t));
-    MultiByteToWideChar(CP_UTF8, 0, utf8, -1, utf16, size_needed);
+    int sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, NULL, 0);
+    wchar_t* utf16 = (wchar_t*) malloc(sizeNeeded * sizeof(wchar_t));
+    MultiByteToWideChar(CP_UTF8, 0, utf8, -1, utf16, sizeNeeded);
     return utf16;
 }
 
 void _logFile(const char * file, unsigned int line)
 {
     const char* format = "%s:%lu ";
-    int size_needed = _snprintf(NULL, 0, format, file, line) + 1;
-    char* buffer = (char*) malloc(size_needed * sizeof(char));
-    _snprintf(buffer, size_needed, format, file, line);
+    int sizeNeeded = _snprintf(NULL, 0, format, file, line) + 1;
+    char* buffer = (char*) malloc(sizeNeeded * sizeof(char));
+    _snprintf(buffer, sizeNeeded, format, file, line);
     OutputDebugStringA(buffer);
     free(buffer);
 }
@@ -45,9 +45,9 @@ void _logFile(const char * file, unsigned int line)
 #define LOG(format, ...) \
     { \
         _logFile( __FILE__, __LINE__); \
-        int size_needed = _snprintf(NULL, 0, format, ##__VA_ARGS__) + 1; \
-        char* buffer = (char*) malloc(size_needed * sizeof(char)); \
-        _snprintf(buffer, size_needed, format, ##__VA_ARGS__); \
+        int sizeNeeded = _snprintf(NULL, 0, format, ##__VA_ARGS__) + 1; \
+        char* buffer = (char*) malloc(sizeNeeded * sizeof(char)); \
+        _snprintf(buffer, sizeNeeded, format, ##__VA_ARGS__); \
         wchar_t* output = _convertToUtf16(buffer); \
         OutputDebugStringW(output); \
         free(buffer); \
--- src/win32/main.cpp
+++ src/win32/main.cpp
@@ -2,6 +2,26 @@
 #include <cstdio>
 #include "diagnostics.h"
 
+LRESULT CALLBACK WindowProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_DESTROY:
+    {
+        PostQuitMessage(0);
+        return 0;
+    }
+    case WM_CLOSE:
+    {
+        DestroyWindow(window);
+        return 0;
+    }
+    default:
+        break;
+    }
+
+    return DefWindowProc(window, message, wParam, lParam);
+}
 
 
 #ifdef TERMINAL_RUN
@@ -14,10 +34,44 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdL
 {
 #endif
     SetupDiagnostics();
-  
-    LOG("elo elo ziąbel\n");
-    LOG("elo elo ziąbel2 %d %s\n", 1, "dwa i trzy");
-    LOG("dwa i trzy\n");
+
+    WNDCLASSEX windowClass = { 0 };
     
+    windowClass.cbSize = sizeof(WNDCLASSEX);
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    windowClass.lpfnWndProc = WindowProc;
+    windowClass.cbClsExtra = 0;
+    windowClass.cbWndExtra = 0;
+    windowClass.hInstance = hInstance;
+    windowClass.hIcon = NULL;
+    windowClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    windowClass.hbrBackground;
+    windowClass.lpszMenuName = NULL;
+    windowClass.lpszClassName = TEXT("a");
+    windowClass.hIconSm = NULL;
+
+    ATOM windowClassAtom = RegisterClassEx(&windowClass);
+
+    HWND window = CreateWindowEx(
+        0,                                         // dwExStyle,
+        (const WCHAR *) windowClassAtom,           // lpClassName,
+        TEXT("Okno zi\x0105""bel"),                // lpWindowName,
+        WS_OVERLAPPEDWINDOW,                       // dwStyle,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,    // X, Y, width, height
+        NULL,                                      // hWndParent
+        NULL,                                      // hMenu
+        hInstance,                                 // hInstance
+        NULL                                       // lpParam
+    );
+    
+    ShowWindow(window, SW_SHOW);
+
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
 }
